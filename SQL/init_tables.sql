7e9d44a46a49e86ec0458d7d967144928c623f0f
g-kakareko <g.kakareko@gmail.com>
2018-06-30 00:31:09 -0400

    all set database

--- SQL/init_tables.sql
+++ SQL/init_tables.sql
@@ -1,46 +1,84 @@
-# STADION: Club	Location	Capacity	stadium_id
-# TRAINER: Name	DOB	Club	trainer_id
-# RUNNER: Name	DOB	Club	gender	trener_id
-# COMPETITON: stadium_id	date	attendence	weather	competition_id
-# RESULT: Name	DOB	Club	gender	result	discipline	date	competition_id	result_id
+-- STADION: Club	Location	Capacity	stadium_id
+-- TRAINER: Name	DOB	Club	trainer_id
+-- RUNNER: Name	DOB	Club	gender	trener_id
+-- COMPETITON: stadium_id	date	attendence	weather	competition_id
+-- RESULT: Name	DOB	Club	gender	result	discipline	date	competition_id	result_id
 CREATE TABLE STADION (
-	Club CHAR,
-	Location CHAR, 
+	Club CHAR(100),
+	Location CHAR(100), 
 	Capacity INT,
 	stadium_id INT,
 	PRIMARY KEY (stadium_id)
 	);
 CREATE TABLE TRAINER (
-	Name CHAR,
+	Name CHAR(100),
 	DOB DATETIME,
-	Club CHAR,
+	Club CHAR(100),
 	trainer_id INT,
 	PRIMARY KEY (trainer_id)
 	);
 CREATE TABLE RUNNER (
-	Name CHAR,
+	Name CHAR(100),
 	DOB DATETIME,
-	Club CHAR,
-	gender VARCHAR(1);
+	Club CHAR(100),
+	gender VARCHAR(1),
 	trener_id INT,
-	PRIMARY KEY (NAME, DOB, gender)
+	PRIMARY KEY (Name, DOB, gender)
 	);
 CREATE TABLE COMPETITON (
 	stadium_id INT,
-	date DATETIME,
-	attendence VARCHAR(1),
-	weather CHAR(8),
-	competition_id CHAR(8),
-	PRIMARY KEY (stadium_id, date)
+	date_comp DATETIME,
+	attendence INT,
+	weather CHAR(1),
+	competition_id INT,
+	PRIMARY KEY (stadium_id, date_comp)
 	);
+-- Name,DOB,Club,gender,result,discipline,date,competition_id,result_id
 CREATE TABLE RESULT (
+	Name CHAR(100),
 	DOB DATETIME ,
-	Club CHAR,
+	Club CHAR(100),
 	gender VARCHAR(1),
 	result FLOAT,
-	discipline CHAR,
-	date DATETIME,
+	discipline CHAR(100),
+	date_comp DATETIME,
 	competition_id INT,
 	result_id INT,
-	PRIMARY KEY (DOB, Club, gender, result, discipline, date, competition_id, result_id)
+	PRIMARY KEY (Name, DOB, Club, gender, result, discipline, date_comp, competition_id, result_id)
 	);
+-- Initializing the Tables:
+-- Stadium
+LOAD DATA LOCAL INFILE '/home/grzegorz/Desktop/Database/Project/database/initial_db/stadion.csv' INTO 
+TABLE STADION FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(Club, Location, Capacity, stadium_id)
+;
+-- Trainer
+LOAD DATA LOCAL INFILE '/home/grzegorz/Desktop/Database/Project/database/initial_db/trainer.csv' INTO 
+TABLE TRAINER FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(Name, @DOB, Club, trainer_id)
+SET DOB = STR_TO_DATE(@DOB, '%m-%d-%y')
+;
+-- Runner
+LOAD DATA LOCAL INFILE '/home/grzegorz/Desktop/Database/Project/database/initial_db/runner.csv' INTO 
+TABLE RUNNER FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(Name, @DOB, Club, gender,trener_id)
+SET DOB = STR_TO_DATE(@DOB, '%m-%d-%y')
+;
+-- Competition
+LOAD DATA LOCAL INFILE '/home/grzegorz/Desktop/Database/Project/database/initial_db/compet.csv' INTO 
+TABLE COMPETITON FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(stadium_id, @date_comp, attendence, weather, competition_id)
+-- SET date_comp = STR_TO_DATE(@date_comp, '%Y-%m-%d')
+SET date_comp = STR_TO_DATE(@date_comp, '%m-%d-%y')
+;
+-- Result
+LOAD DATA LOCAL INFILE '/home/grzegorz/Desktop/Database/Project/database/initial_db/result60.csv' INTO 
+TABLE RESULT FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
+IGNORE 1 ROWS
+(Name, @DOB, Club, gender, result, discipline, @date_comp, competition_id, result_id) 
+SET DOB = STR_TO_DATE(@DOB, '%m-%d-%y'), date_comp = STR_TO_DATE(@date_comp, '%m-%d-%y')
+;
